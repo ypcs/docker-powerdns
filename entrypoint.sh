1b693d042ad41ea79b594f855b7b68e489a359a1
Ville Korhonen <ville@xd.fi>
2019-04-16 09:34:15 +0300

    Configure pdns database connection

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,6 +11,17 @@ then
 	-e 's/CREATE INDEX/CREATE INDEX IF NOT EXISTS/g' \
 	-e 's/CREATE UNIQUE INDEX/CREATE UNIQUE INDEX IF NOT EXISTS/g' \
 	"${SCHEMA}" |mysql -u "${MYSQL_USER}" --password="${MYSQL_PASSWORD}" "${MYSQL_DATABASE}" 
+
+    cat >> /etc/pdns/pdns.conf << EOF
+#
+# Runtime database configuration
+#
+launch=gmysql
+gmysql-host=${MYSQL_HOST}
+gmysql-user=${MYSQL_USER}
+gmysql-dbname=${MYSQL_DATABASE}
+gmysql-password=${MYSQL_PASSWORD}
+EOF
 fi
 
 echo "I: Starting pdns server."
