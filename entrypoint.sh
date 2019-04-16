fbef383c226862c475b8c20bd08293967c5ec711
Ville Korhonen <ville@xd.fi>
2019-04-16 07:54:53 +0300

    Import schema to database on start

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
 set -e
 
+SCHEMA="/usr/share/pdns-backend-mysql/schema/schema.mysql.sql"
+
+if [ \( -n "${MYSQL_DATABASE}" -a -n "${MYSQL_USER}" -a -n "${MYSQL_PASSWORD}" \) ]
+then
+    echo "I: Create database schema (if not exists)."
+    sed \
+        -e 's/^CREATE DATABASE/CREATE DATABASE IF NOT EXISTS/g' \
+	-e 's/CREATE INDEX/CREATE INDEX IF NOT EXISTS/g' \
+	-e 's/CREATE UNIQUE INDEX/CREATE UNIQUE INDEX IF NOT EXISTS/g' \
+	"${SCHEMA}" |mysql -u "${MYSQL_USER}" --password="${MYSQL_PASSWORD}" "${MYSQL_DATABASE}" 
+fi
+
+echo "I: Starting pdns server."
 pdns_server "$@"
